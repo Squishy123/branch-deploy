421d0d6e754c1b7dc20c7dfc37bb46ad15145ef3
squishy123 <christian.gnaw@gmail.com>
2019-06-02 16:10:05 -0400

    Added prettier formatting and changes to job status api

--- src/main.js
+++ src/main.js
@@ -20,11 +20,11 @@ db.read();
  * branch_name
  * branch_id //unique, generated shortid.generate()
  * }
- * 
+ *
  * Host
  * {
  * host_name //unique from branch_id
- * host_port 
+ * host_port
  * branch_id //related branch_id
  * }
  */
@@ -53,6 +53,7 @@ let router = express.Router();
 router.use((req, res, next) => {
     req.data = {};
     req.data = Object.assign(req.data, req.params, req.body, req.query);
+    console.log(req.data);
     next();
 });
 
@@ -68,29 +69,45 @@ router.post('/open', async (req, res) => {
     try {
         if (!req.data.branch_name)
             return res.send({
-                status: 'ERROR Missing required param: branch_name'
+                status: 'ERROR Missing required param: branch_name',
             });
-
 
         let id = shortid.generate().toLowerCase();
 
-        db.get('jobs').push({ branch_id: id, status: 'pending' }).write();
+        db.get('jobs')
+            .push({
+                branch_id: id,
+                branch_name: req.data.branch_name,
+                status: 'pending',
+            })
+            .write();
 
         //clone repo in dir with id
-        Git.Clone(process.env.REPO_URL, `./branches/${req.data.branch_name}_build_${id}`)
-            .then(function (repo) {
-                console.log("Completed clone of repo!");
+        Git.Clone(
+            process.env.REPO_URL,
+            `./branches/${req.data.branch_name}_build_${id}`
+        )
+            .then(function(repo) {
+                console.log('Completed clone of repo!');
                 db.get('active_branches')
                     .push({
                         branch_name: req.data.branch_name,
                         branch_id: id,
-                        path: `branches/${req.data.branch_name}_build_${id}`
-                    }).write();
+                        path: `branches/${req.data.branch_name}_build_${id}`,
+                    })
+                    .write();
 
-                db.get('jobs').find({ branch_id: id }).set('status', 'completed').write();
+                db.get('jobs')
+                    .find({ branch_id: id })
+                    .set('status', 'completed')
+                    .write();
             })
-            .catch(function (err) {
-                db.get('jobs').find({ branch_id: id }).set('status', 'cancelled').set('message', `ERROR ${err}`).write();
+            .catch(function(err) {
+                db.get('jobs')
+                    .find({ branch_id: id })
+                    .set('status', 'cancelled')
+                    .set('message', `ERROR ${err}`)
+                    .write();
             });
 
         return res.send({
@@ -98,12 +115,11 @@ router.post('/open', async (req, res) => {
             branch_id: id,
             branch_name: req.data.branch_name,
             job: `/status/${id}`,
-            job_status: 'pending'
+            job_status: 'pending',
         });
-
     } catch (err) {
         return res.send({
-            status: `ERROR ${err}`
+            status: `ERROR ${err}`,
         });
     }
 });
@@ -111,27 +127,28 @@ router.post('/open', async (req, res) => {
 router.get('/status/:branch_id', (req, res) => {
     return res.send({
         status: 'OK',
-        job: db.get('jobs').find({ branch_id: req.params.branch_id }).value()
+        job: db
+            .get('jobs')
+            .find({ branch_id: req.params.branch_id })
+            .value(),
     });
 });
 
 /**
  * Closes a branch and stops serving it
- * 
+ *
  */
-router.post('/close', (req, res) => {
-
-})
+router.post('/close', (req, res) => {});
 
 router.get('/active_branches', (req, res) => {
     try {
         res.send({
             status: 'OK',
-            active_branches: db.get('active_branches')
+            active_branches: db.get('active_branches'),
         });
     } catch (err) {
         return res.send({
-            status: `ERROR ${err}`
+            status: `ERROR ${err}`,
         });
     }
 });
@@ -140,11 +157,11 @@ router.get('/closed_branches', (req, res) => {
     try {
         res.send({
             status: 'OK',
-            closed_branches: db.get('closed_branches')
+            closed_branches: db.get('closed_branches'),
         });
     } catch (err) {
         return res.send({
-            status: `ERROR ${err}`
+            status: `ERROR ${err}`,
         });
     }
 });
@@ -155,20 +172,28 @@ router.get('/branches', (req, res) => {
             status: 'OK',
             branches: {
                 active_branches: db.get('active_branches'),
-                closed_branches: db.get('closed_branches')
-            }
+                closed_branches: db.get('closed_branches'),
+            },
         });
     } catch (err) {
         return res.send({
-            status: `ERROR ${err}`
+            status: `ERROR ${err}`,
         });
     }
 });
 
 server.use('/branch-deploy', router);
 
-server.listen((process.env.PORT) ? process.env.PORT : 3000, process.env.HOST ? process.env.HOST : 'localhost', function () {
-    console.log(`${server.name} listening at http://${process.env.HOST ? process.env.HOST : 'localhost'}:${(process.env.PORT) ? process.env.PORT : 3000}`);
-});
+server.listen(
+    process.env.PORT ? process.env.PORT : 3000,
+    process.env.HOST ? process.env.HOST : 'localhost',
+    function() {
+        console.log(
+            `${server.name} listening at http://${
+                process.env.HOST ? process.env.HOST : 'localhost'
+            }:${process.env.PORT ? process.env.PORT : 3000}`
+        );
+    }
+);
 
 //start up stored branches
